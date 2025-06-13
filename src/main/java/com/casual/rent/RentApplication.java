33c8a229912514131177c7ea2b0714583d94fce6
PENG <jcpeng3-c@my.cityu.edu.hk>
2025-06-13 23:53:37 +0800

    update

--- src/main/java/com/casual/rent/RentApplication.java
+++ src/main/java/com/casual/rent/RentApplication.java
@@ -11,7 +11,8 @@ public class RentApplication {
     public static void main(String[] args) {
         SpringApplication.run(RentApplication.class, args);
         System.out.println("===========================================");
-        System.out.println("Casual Rent启动成功！");
+        System.out.println("Casual Rent 后端启动成功！");
+        System.out.println("接口文档地址：http://localhost:8080/api/swagger-ui.html");
         System.out.println("===========================================");
     }
 } 
